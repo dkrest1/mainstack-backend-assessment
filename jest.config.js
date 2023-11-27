8200ee2924f3dc7366d8cd1c0bf8f971e86e7e2f
Oluwatosin Akande <aktosin1@gmail.com>
2023-11-27 20:18:56 +0100

    update: added eslint and prettier confiuration

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    restoreMocks: true,
+    collectCoverage: false,
+    coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
+    coverageReporters: ['text', 'lcov', 'clover', 'html'],
+    testMatch: ["**/tests/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"]
 };
